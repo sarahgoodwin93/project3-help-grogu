5ddaf5ead45fc1fdfbf1f45f6cea75e120a15ff4
sarahgoodwin93 <>
2023-08-27 15:43:48 +0000

    FEAT: add welcome message star-wars- style

--- run.py
+++ run.py
@@ -5,9 +5,10 @@ def intro_text():
     and explains the story and journey they are about to go on
     """
     print(images.logo_img)
-    print("Welcome to the command-line game that will take you on an out of space adventure")
+    print("Welcome to the command-line game that will take you on an out of space adventure!")
     print("Can you help Grogu find his way back to Din Djarin?")
 
+intro_text()
+print(images.welcome_message)
 username = input("What is your name? ")
 print(username)
-
