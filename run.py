705fbe3d8d6a9a5e4c73950d8a61bd68bc92b01b
sarahgoodwin93 <>
2023-08-27 15:36:53 +0000

    Add intro text function & username input

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import images #contains ASCII images used in the application 
+
+def intro_text():
+    """Introduction to the game, allows users to input their name, 
+    and explains the story and journey they are about to go on
+    """
+    print(images.logo_img)
+    print("Welcome to the command-line game that will take you on an out of space adventure")
+    print("Can you help Grogu find his way back to Din Djarin?")
+
+username = input("What is your name? ")
+print(username)
+
