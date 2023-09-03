2d9346a2e6b9056024a0fc3963a3139f8e621ad7
sarahgoodwin93 <>
2023-09-03 13:36:04 +0000

    DEPLOY: deployed to heroku and added username output

--- run.py
+++ run.py
@@ -1,14 +1,20 @@
-import images #contains ASCII images used in the application 
+# Imports 
+import images 
+# Contains ASCII images used in the application 
 
+
+# Game Intro Text
 def intro_text():
-    """Introduction to the game, allows users to input their name, 
+    """
+    Introduction to the game, allows users to input their name 
     and explains the story and journey they are about to go on
     """
     print(images.logo_img)
-    print("Welcome to the command-line game that will take you on an out of space adventure!")
+    print("Welcome to the game that will take you on an out of space adventure!")
     print("Can you help Grogu find his way back to Din Djarin?")
+
 
 intro_text()
 print(images.welcome_message)
 username = input("What is your name? ")
-print(username)
+print(f"Welcome {username}, are you ready to begin?")
