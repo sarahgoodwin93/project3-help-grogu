d2abe727df0d2630604f23e93e73ecfbf810ca9a
sarahgoodwin93 <>
2023-09-03 14:05:05 +0000

    FEAT: Add first challenge options

--- run.py
+++ run.py
@@ -9,12 +9,22 @@ def intro_text():
     Introduction to the game, allows users to input their name 
     and explains the story and journey they are about to go on
     """
-    print(images.logo_img)
     print("Welcome to the game that will take you on an out of space adventure!")
     print("Can you help Grogu find his way back to Din Djarin?")
+    print(images.logo_img)
 
 
 intro_text()
 print(images.welcome_message)
-username = input("What is your name? ")
-print(f"Welcome {username}, are you ready to begin?")
+username = input("What is your name?\n")
+print(f"Welcome {username}, let's begin")
+
+first_challenge()
+print("You discover that Mando might be on Moth Gideons light cruiser")
+print("BUt you need a ship to be able to catch up to him")
+print("Do you...")
+print("A) Try to win a ship in a card game with sketchy gamblers at the local drinking spot?")
+print("B) Try to sneak onboard a ship leaving the shipyard as stowaways?")
+print("C) Get Grogu to steal someone's keys using the force")
+user_choice = str(input("What do you deciede A,B or C? \n"))
+if user_choice == 
