7e362a32a5063a4839a8d4fa29c869cc5d6689ef
sarahgoodwin93 <>
2023-09-13 13:57:10 +0000

    FEAT: add other 5 challenge wording structure

--- run.py
+++ run.py
@@ -18,10 +18,10 @@ def intro_text():
         username = input("")
         if not username.isalpha():
             print("Sorry, please use letters we can understand")
-            continue
         else:
             print(f"Welcome {username}, let's begin")
-            continue
+    first_challenge()
+    second_challenge()
 
 
 def first_challenge():
@@ -51,12 +51,12 @@ def second_challenge():
     Outline the second options of the game and allows user to choose where the game will go
     """
     user_choice = ""
-    print("You discover that Mando might be on Moth Gideons light cruiser")
-    print("But you need a ship to be able to catch up to him")
+    print("You make it onto the ship")
+    print("But the ship doesnt start")
     print("Do you...")
-    print("A) Try to win a ship in a card game with sketchy gamblers at the local drinking spot?")
-    print("B) Try to sneak onboard a ship leaving the shipyard as stowaways?")
-    print("C) Get Grogu to steal someone's keys using the force")
+    print("A) Call the mechanic")
+    print("B) Try to fix it yours")
+    print("C) Get different ship")
     user_choice = str(input("What do you deciede A,B or C? \n"))
     if user_choice == "A":
         print("")
@@ -73,12 +73,12 @@ def third_challenge():
     Outline the second options of the game and allows user to choose where the game will go
     """
     user_choice = ""
-    print("You discover that Mando might be on Moth Gideons light cruiser")
-    print("But you need a ship to be able to catch up to him")
+    print("Get to planet and go to salon to find more infomation")
+    print("Moth Giden trops have been spotted on the other side of the planet")
     print("Do you...")
-    print("A) Try to win a ship in a card game with sketchy gamblers at the local drinking spot?")
-    print("B) Try to sneak onboard a ship leaving the shipyard as stowaways?")
-    print("C) Get Grogu to steal their keys using the force")
+    print("A) Get speedster")
+    print("B) Get back on ship and try to fly")
+    print("C) Get distracted by eggs")
     user_choice = str(input("What do you deciede A,B or C? \n"))
     if user_choice == "A":
         print("")
@@ -90,14 +90,76 @@ def third_challenge():
         print("You must choice A, B or C")
 
 
+def fourth_challenge():
+    """
+    Outline the second options of the game and allows user to choose where the game will go
+    """
+    user_choice = ""
+    print("Get to planet and go to salon to find more infomation")
+    print("Moth Giden trops have been spotted on the other side of the planet")
+    print("Do you...")
+    print("A) Get speedster")
+    print("B) Get back on ship and try to fly")
+    print("C) Get distracted by eggs")
+    user_choice = str(input("What do you deciede A,B or C? \n"))
+    if user_choice == "A":
+        print("")
+    elif user_choice == "B":
+        print("")
+    elif user_choice == "C":
+        print("")
+    else:
+        print("You must choice A, B or C")
+
+
+def fifth_challenge():
+    """
+    Outline the second options of the game and allows user to choose where the game will go
+    """
+    user_choice = ""
+    print("Get to other side of planet and they are not to be seen")
+    print("But you find Mandos helmet")
+    print("Do you...")
+    print("A) Use the force to try to locate him")
+    print("B) Cry and go home")
+    print("C) Go and get the covenant to help")
+    user_choice = str(input("What do you deciede A,B or C? \n"))
+    if user_choice == "A":
+        print("")
+    elif user_choice == "B":
+        print("")
+    elif user_choice == "C":
+        print("")
+    else:
+        print("You must choice A, B or C")
+
+
+def sixth_challenge():
+    """
+    Outline the second options of the game and allows user to choose where the game will go
+    """
+    user_choice = ""
+    print("You have used the force and found he is still on the planet")
+    print("Taking your speedster to go to where they are hiding")
+    print("Do you...")
+    print("A) Break in and resuce him")
+    print("B) Wait until he is transported out to attack then")
+    print("C) Wait for help from the covenant")
+    user_choice = str(input("What do you deciede A,B or C? \n"))
+    if user_choice == "A":
+        print("")
+    elif user_choice == "B":
+        print("")
+    elif user_choice == "C":
+        print("")
+    else:
+        print("You must choice A, B or C")
+
 def main():
     """
     Main functions in order
     """
     intro_text()
-    first_challenge()
-    second_challenge()
-    third_challenge()
 
 
 main()
